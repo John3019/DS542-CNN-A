99b2b17965817c4a270495c8a4d9a163c9a68368
Atul Aravind das <atuladas@scc1.bu.edu>
2025-04-05 16:06:25 -0400

    Updated the file OneImageLabelExample.py and created a new file to get labels for all images

---
+++ GetAllLabels.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import pydicom
+import os
+
+files_path='/projectnb/dl4ds/students/atuladas/DS542-CNN-A/Midline Train Test/train'
+patient_details_df=pd.read_csv('/projectnb/dl4ds/students/atuladas/DS542-CNN-A/Project-Datase.csv')
+
+
--- OneImageLabelExample.py
+++ OneImageLabelExample.py
@@ -5,16 +5,17 @@ import os
 
 
 patient_details_df=pd.read_csv('/projectnb/dl4ds/students/atuladas/DS542-CNN-A/Project-Datase.csv')
-dicom_path='/projectnb/dl4ds/students/atuladas/DS542-CNN-A/Midline Train Test/train/941_S_1363_2007-03-12_Slice_81.dcm'
+dicom_path='/projectnb/dl4ds/students/atuladas/DS542-CNN-A/Midline Train Test/train/006_S_4192_2011-09-27_Slice_86.dcm'
 file_path=os.path.basename(dicom_path)
 subject_id='_'.join(file_path.split('_')[:3])
 print(subject_id)
-print(patient_details_df.head())
+
 
 matching_row=patient_details_df[patient_details_df['Subject']==subject_id]
-
+print(matching_row)
 if not matching_row.empty:
     label_info = matching_row.iloc[0].to_dict()
+    print(label_info)
     print(f"✅ Match found for subject {subject_id}:\n")
     print("Subject:",label_info["Subject"],"\n")
     print("Diagnosis:",label_info["Group"],"\n")
