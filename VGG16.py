9c1492c0a741e722c8d33f20fb72875c6ae7f7a2
Atul Aravind das <atuladas@scc1.bu.edu>
2025-04-05 19:28:06 -0400

    Wrote a small code snippet to determine the shape of a sample image in the datasets

--- VGG16.py
+++ VGG16.py
@@ -15,3 +15,28 @@ from PIL import Image
 from tqdm.auto import tqdm
 import wandb
 import argparse
+
+class AlzheimerDataset(Dataset):
+    def __init__(self,image_dir,csv_path,transform=None):
+
+
+def main():
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--learning_rate", type=float, default=0.001)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--optimizer", type=str, default="adam", choices=["adam", "sgd"])
+    parser.add_argument("--epochs", type=int, default=10)
+    args = parser.parse_args()
+    
+    CONFIG = {
+        "model": "VGG16_Alzheimers",
+        "batch_size": args.batch_size,
+        "learning_rate": args.learning_rate,
+        "optimizer": args.optimizer,
+        "epochs": args.epochs,
+        "num_workers": 4,
+        "device": "cuda" if torch.cuda.is_available() else "cpu",
+        "data_dir": "/projectnb/dl4ds/students/atuladas/DS542-CNN-A/Midline Train Test/train",
+        "csv_path": "Project-Datase.csv",
+        "wandb_project": "Danke Thomas-Müller"
+    }
